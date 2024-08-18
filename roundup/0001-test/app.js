a334b6687eae47990c844c576975f0a30d60de84
Aldo Lugo <aldo@aldomatic.com>
2024-08-18 16:24:58 -0500

    test

--- roundup/0001-test/app.js
+++ roundup/0001-test/app.js
@@ -1,3 +1,10 @@
 function runMe() {
     console.log('Hello!')
 }
+
+const q1 = ["Jan", "Feb", "Mar"];
+const q2 = ["Apr", "May", "Jun"];
+const q3 = ["Jul", "Aug", "Sep"];
+const q4 = ["Oct", "Nov", "May"];
+
+const year = [...q1, ...q2, ...q3, ...q4];
